85146aa966217a06d9c29571c1872bf7b6741414
Alexander Ho <alexyho9@gmail.com>
2023-07-18 19:06:06 -0700

    Feature 12 Complete

--- todos/urls.py
+++ todos/urls.py
@@ -5,6 +5,7 @@ from todos.views import (
     create_todo_list,
     update_todo_list,
     delete_todo_list,
+    create_todo_item,
 )
 
 
@@ -14,4 +15,5 @@ urlpatterns = [
     path("create/", create_todo_list, name="create_todo_list"),
     path("<int:id>/edit/", update_todo_list, name="update_todo_list"),
     path("<int:id>/delete/", delete_todo_list, name="delete_todo_list"),
+    path("items/create/", create_todo_item, name="create_todo_item"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from todos.models import TodoList
-from todos.forms import TodoListForm
+from todos.forms import TodoListForm, TodoItemForm
 
 
 # Create your views here.
@@ -58,3 +58,18 @@ def delete_todo_list(request, id):
         tasks.delete()
         return redirect("todo_list_list")
     return render(request, "todos/delete.html")
+
+
+def create_todo_item(request):
+    if request.method == "POST":
+        form = TodoItemForm(request.POST)
+        if form.is_valid():
+            item = form.save()
+            return redirect("todo_list_detail", id=item.list.id)
+    else:
+        form = TodoItemForm()
+
+    context = {
+        "item_form": form,
+    }
+    return render(request, "todos/create_item.html", context)
